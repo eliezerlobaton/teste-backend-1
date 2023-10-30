1f81c86358fb02939dee2751043a8b257282e578
Eliezer Lobaton <eliezerlobaton39@gmail.com>
2023-10-30 13:00:41 -0300

    refactor: separação de layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    @vite('resources/css/app.css')
-
-</head>
-
-<body class="antialiased">
+<x-layout>
     <div class="container mx-auto p-4">
         <h1 class="text-2xl font-bold mb-4">Noticias</h1>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -31,6 +14,4 @@
             @endforeach
         </div>
     </div>
-</body>
-
-</html>
+</x-layout>
